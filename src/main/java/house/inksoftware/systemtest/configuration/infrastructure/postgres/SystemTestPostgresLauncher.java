1e68605c5decf89d4eda6cd30235842414fb3d6c
Tomasz Wozniczka <tomasz.wozniczka@inksoftware.house>
2022-09-22 13:51:05 +0200

    Adjust framework

--- src/main/java/house/inksoftware/systemtest/configuration/infrastructure/postgres/SystemTestPostgresLauncher.java
+++ src/main/java/house/inksoftware/systemtest/configuration/infrastructure/postgres/SystemTestPostgresLauncher.java
@@ -1,42 +1,33 @@
 package house.inksoftware.systemtest.configuration.infrastructure.postgres;
 
+import com.google.common.base.Preconditions;
 import house.inksoftware.systemtest.configuration.infrastructure.SystemTestResourceLauncher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
+
+import static java.util.Objects.nonNull;
 
 public class SystemTestPostgresLauncher implements SystemTestResourceLauncher {
     private static final Logger LOGGER = LoggerFactory.getLogger(SystemTestPostgresLauncher.class);
 
-    private static PostgreSQLContainer container;
+    private final PostgreSQLContainer container;
     private final SystemTestPostgresConfiguration postgresConfiguration;
 
-    public SystemTestPostgresLauncher(SystemTestPostgresConfiguration postgresConfiguration) {
+    public SystemTestPostgresLauncher(PostgreSQLContainer container, SystemTestPostgresConfiguration postgresConfiguration) {
+        this.container = container;
         this.postgresConfiguration = postgresConfiguration;
     }
 
     @Override
     public void setup() {
-        if (container == null) {
-            DockerImageName myImage = DockerImageName.parse(postgresConfiguration.getImage()).asCompatibleSubstituteFor("postgres");
-            container = new PostgreSQLContainer(myImage);
-            container.start();
-            System.setProperty("DB_URL", container.getJdbcUrl());
-            System.setProperty("DB_USERNAME", container.getUsername());
-            System.setProperty("DB_PASSWORD", container.getPassword());
-            LOGGER.info(
-                    "Starting test database... jdbc: {}, user: {}, password: {}",
-                    container.getJdbcUrl(), container.getUsername(), container.getPassword()
-            );
-
-            postgresConfiguration.getInitialDataPopulation().populate();
-        }
+        Preconditions.checkState(nonNull(container), "Container not initialized");
+        postgresConfiguration.getInitialDataPopulation().populate();
     }
 
     @Override
     public void shutdown() {
-        if (container == null) {
+        if (container != null) {
             LOGGER.info("Shutting down test database...");
             container.stop();
             container.close();
