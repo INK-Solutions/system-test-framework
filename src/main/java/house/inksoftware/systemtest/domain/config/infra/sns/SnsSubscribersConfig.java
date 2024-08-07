d4654c29745cb433f4a1a6e210cf6ad25bea81ee
VladBrok <vlad.brok99@gmail.com>
2024-08-07 13:07:16 +0200

    refactor

--- src/main/java/house/inksoftware/systemtest/domain/config/infra/sns/SnsSubscribersConfig.java
+++ src/main/java/house/inksoftware/systemtest/domain/config/infra/sns/SnsSubscribersConfig.java
@@ -1,12 +1,19 @@
 package house.inksoftware.systemtest.domain.config.infra.sns;
 
 import java.util.function.Supplier;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
-@Data
 @RequiredArgsConstructor
 public class SnsSubscribersConfig {
+    
     private final Supplier<SqsClient> sqsClientSupplier;
+    private SqsClient sqsClient;
+    
+    public SqsClient sqsClient() {
+        if (sqsClient == null) {
+            sqsClient = sqsClientSupplier.get();
+        }
+        return sqsClient;
+    }
 }
--- src/main/java/house/inksoftware/systemtest/domain/config/infra/sns/SnsTopicFactory.java
+++ src/main/java/house/inksoftware/systemtest/domain/config/infra/sns/SnsTopicFactory.java
@@ -3,35 +3,77 @@ package house.inksoftware.systemtest.domain.config.infra.sns;
 import static house.inksoftware.systemtest.domain.sns.SnsTopicDefinition.Type.FIFO;
 import static house.inksoftware.systemtest.domain.sns.SnsTopicDefinition.Type.STANDARD;
 
+import house.inksoftware.systemtest.domain.config.infra.sqs.queue.SqsQueueFactory;
 import house.inksoftware.systemtest.domain.sns.SnsTopicDefinition;
+import house.inksoftware.systemtest.domain.sns.SnsTopicDefinition.Protocol;
+import house.inksoftware.systemtest.domain.sqs.queue.SqsQueueDefinition;
+import house.inksoftware.systemtest.domain.sqs.queue.SqsQueueDefinition.Type;
 import java.util.List;
 import java.util.Map;
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.CreateTopicRequest;
+import software.amazon.awssdk.services.sns.model.SubscribeRequest;
+import software.amazon.awssdk.services.sqs.model.GetQueueAttributesRequest;
+import software.amazon.awssdk.services.sqs.model.QueueAttributeName;
 
 public class SnsTopicFactory {
 
-    public void create(SnsClient snsClient, List<SnsTopicDefinition> topicDefinitions) {
-        topicDefinitions.forEach(topicDefinition -> create(snsClient, topicDefinition));
+    public static List<SnsCreatedTopicResult> create(SnsClient snsClient, List<SnsTopicDefinition> topicDefinitions, SnsSubscribersConfig subscribersConfig) {
+        return topicDefinitions.stream()
+            .map(topic -> create(snsClient, topic, subscribersConfig))
+            .toList();
     }
 
-    private void create(SnsClient snsClient, SnsTopicDefinition topicDefinition) {
+    private static SnsCreatedTopicResult create(SnsClient snsClient, SnsTopicDefinition topic, SnsSubscribersConfig subscribersConfig) {
         var createTopicRequestBuilder = CreateTopicRequest.builder()
-            .name(topicDefinition.fullName());
+            .name(topic.fullName());
         
-        if (topicDefinition.getType().equals(FIFO)) {
+        if (topic.getType().equals(FIFO)) {
             createTopicRequestBuilder.attributes(Map.of(
                     "FifoTopic", "true",
                     "ContentBasedDeduplication", "true"
             ));
-        } else if (topicDefinition.getType().equals(STANDARD)) {
+        } else if (topic.getType().equals(STANDARD)) {
             createTopicRequestBuilder.attributes(Map.of());
         } else {
-            throw new IllegalArgumentException("Not supported SNS topic type: " + topicDefinition.getType());
+            throw new IllegalArgumentException("Not supported SNS topic type: " + topic.getType());
         }
         
         var request = createTopicRequestBuilder.build();
         var response = snsClient.createTopic(request);
+        var result = SnsCreatedTopicResult.builder();
+        
+        if (topic.getProtocol().equals(Protocol.SQS)) {
+            result.sqsSubscriber(createDefaultSqsSubscriber(snsClient, subscribersConfig, topic, response.topicArn()));
+        }
+        
+        return result.build();
+    }
+    
+    private static SqsQueueDefinition createDefaultSqsSubscriber(SnsClient snsClient, SnsSubscribersConfig subscribersConfig, SnsTopicDefinition topic, String topicArn) {
+        var result = new SqsQueueDefinition(topic.defaultSubscriberName(), Type.fromShortName(topic.getType().getShortName()));
+        
+        var createdQueue = SqsQueueFactory.create(subscribersConfig.sqsClient(), result);
+        var attributesRequest = GetQueueAttributesRequest.builder()
+            .queueUrl(createdQueue.queueUrl())
+            .attributeNames(List.of(QueueAttributeName.QUEUE_ARN))
+            .build();
+        String queueArn = subscribersConfig.sqsClient().getQueueAttributes(attributesRequest).attributes().get(QueueAttributeName.QUEUE_ARN);
+        var subscribeRequest = buildSubscribeRequest(topicArn, queueArn, "sqs");
+        snsClient.subscribe(subscribeRequest);
+        
+        return result;
     }
 
+    private static SubscribeRequest buildSubscribeRequest(String topicArn, String endpointArn, String protocol) {
+        return SubscribeRequest.builder()
+                .protocol(protocol)
+                .endpoint(endpointArn)
+                .returnSubscriptionArn(true)
+                .attributes(Map.of(
+                    "RawMessageDelivery", "true"
+                ))
+                .topicArn(topicArn)
+                .build();
+    }
 }
