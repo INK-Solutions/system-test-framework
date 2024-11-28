151af995ebeac18458a758b3b81c1bd06498fa1f
GeorgeSam98 <61777347+GeorgeSam98@users.noreply.github.com>
2024-11-28 11:49:18 +0300

    Feature/spring kafka upgrade (#25)
    * remove .gradle tracking
    
    * - upgraded spring kafkas
    - upgraded serde
    
    ---------
    
    Co-authored-by: George Samanlian <gsamanlian@seedz.tech>

--- src/main/java/house/inksoftware/systemtest/domain/config/infra/kafka/outgoing/MockedKafkaAvroValueDeserializer.java
+++ src/main/java/house/inksoftware/systemtest/domain/config/infra/kafka/outgoing/MockedKafkaAvroValueDeserializer.java
@@ -5,12 +5,19 @@ import io.confluent.kafka.schemaregistry.avro.AvroSchema;
 import io.confluent.kafka.schemaregistry.client.MockSchemaRegistryClient;
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
 import org.apache.avro.Schema;
+import org.apache.kafka.common.header.Headers;
 
 public abstract class MockedKafkaAvroValueDeserializer extends KafkaAvroDeserializer {
     @Override
     public Object deserialize(String topic, byte[] bytes) {
         this.schemaRegistry = getMockClient(toSchema(topic));
         return super.deserialize(topic, bytes);
+    }
+
+    @Override
+    public Object deserialize(String topic, Headers headers, byte[] bytes) {
+        this.schemaRegistry = getMockClient(toSchema(topic));
+        return super.deserialize(topic, headers, bytes);
     }
 
     public abstract Schema toSchema(String topic);
